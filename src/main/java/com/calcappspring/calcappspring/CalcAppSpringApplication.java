0c42b16ae67ad1ceaf2a4250c54d45c8f73dfc24
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-03-17 03:31:45 +0300

    initial

---
+++ src/main/java/com/calcappspring/calcappspring/CalcAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.calcappspring.calcappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcAppSpringApplication.class, args);
+    }
+
+}
